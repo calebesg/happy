c29b04a6228fededdfb0654897febf0912a7cfc2
CalebeSG <calebeguimaraes10@gmail.com>
2020-10-14 19:09:15 -0300

    relationship between orphanage and image created

--- backend/src/models/Orphanage.ts
+++ backend/src/models/Orphanage.ts
@@ -1,4 +1,12 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { 
+  Entity, 
+  Column, 
+  PrimaryGeneratedColumn, 
+  OneToMany, 
+  JoinColumn 
+} from 'typeorm';
+
+import Image from './Image';
 
 @Entity('orphanages')
 export default class Orphanage {
@@ -26,5 +34,11 @@ export default class Orphanage {
 
   @Column()
   open_on_weekends: string;
+
+  @OneToMany(() => Image, image => image.orphanage, {
+    cascade: ['insert', 'update']
+  })
+  @JoinColumn({ name: 'orphanage_id' })
+  images: Image[];
 
 }
